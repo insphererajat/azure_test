a6b2acf97947e46cf1a9f03a40d736f4c247c1dd
insphererajat <101311260+insphererajat@users.noreply.github.com>
2022-03-11 12:08:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php  
-  echo"i am rajat chauahan. I am devops eng. And i have been working Insphere solution pvt. ltd.";
+  echo"i am rajat chauahan. I am devops eng. And i have been working Insphere solution pvt. ltd. i have work on app service";
 ?>
